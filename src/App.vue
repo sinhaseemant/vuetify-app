f3d5c25f3078528a89dc38801188fd4e46bcd22d
Seemant Sinha <sinhaseemant@gmail.com>
2022-03-09 04:10:10 +0530

    added static flex box

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn class="mx-2" fab dark small color="primary">
+        <v-icon dark> mdi-heart </v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-layout row wrap>
+          <v-flex lg-6 md-6>
+            <InputCard />
+          </v-flex>
+          <v-flex lg-6 md-6>
+            <InputCard />
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+          <v-flex lg-6 md-6>
+            <InputCard />
+          </v-flex>
+          <v-flex lg-6 md-6>
+            <InputCard />
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+          <v-flex lg-6 md-6>
+            <InputCard />
+          </v-flex>
+          <v-flex lg-6 md-6>
+            <InputCard />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputCard from "./components/Input-Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    InputCard,
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -25,4 +79,33 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.card {
+  background-color: #ecf0f1;
+  border-radius: 4px;
+  margin: 0 auto;
+  padding: 0 20px 20px;
+  width: 360px;
+  margin-top: 20px;
+}
+input {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 1em;
+  padding: 10px;
+  margin: 10px 5px;
+  border: 0;
+  border-bottom: 1px solid #757575;
+}
+button {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 1em;
+  padding: 10px;
+  margin: 10px 5px;
+  border: 0;
+  border-radius: 3px;
+  background-color: #3498db;
+  color: #ecf0f1;
+}
+#remove {
+  background-color: #e74c3c;
+} */
 </style>
---
+++ src/components/Input-Card.vue
@@ -0,0 +1,87 @@
+<template>
+  <!-- <v-container>
+    <center> -->
+  <div>
+    <v-card v-for="index in list" v-bind:key="index" elevation="3" class="card">
+      <div class="subtitle">
+        <div class="input">
+          <v-text-field
+            label="Username"
+            type="text"
+            id="username"
+            name="username"
+            v-model="index.username"
+          />
+        </div>
+        <v-text-field
+          label="Email"
+          type="email"
+          id="email"
+          name="email"
+          v-model="index.email"
+        />
+      </div>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="add()" color="success">Add</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn @click="remove(index)" color="error">Remove</v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+
+      <!-- <p v-for="user in list" v-bind:key="user" ref="user">
+      UserName: {{ user.username }} <br />
+      Email: {{ user.email }}
+    </p>  -->
+    </v-card>
+  </div>
+  <!-- </center>
+  </v-container> -->
+</template>
+
+<script>
+export default {
+  name: "InputCard",
+  data() {
+    return {
+      list: [
+        {
+          username: "",
+          email: "",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    add: function () {
+      this.list.push({
+        username: "",
+        email: "",
+      });
+    },
+    remove: function (index) {
+      this.list.splice(this.list.indexOf(index), 1);
+    },
+  },
+};
+</script>
+
+<style>
+.subtitle {
+  /* height: 10px; */
+  height: 170px;
+
+  padding: 5px 10%;
+  align-items: center;
+}
+.card {
+  height: 230px;
+  width: 100%;
+  margin: 5px 5px;
+}
+.v-card {
+  width: 100%;
+  margin: 10px 10px;
+}
+</style>
